d62abd1da192835956b0f29ba29ea4e05bdcdc7f
AdenilsonAleixo <adenilson.r.aleixo@gmail.com>
2017-06-22 22:22:41 -0300

    Realizado teste para salvar usuario via formulario

--- src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
+++ src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package br.com.fabricadeprogramador.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,13 +9,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.fabricadeprogramador.persistencia.entidade.Usuario;
+import br.com.fabricadeprogramador.persistencia.jdbc.UsuarioDAO;
+
 //http://localhost:8080/fabricaweb/usucontroller.do
-@WebServlet(name="/usucontroller")
+@WebServlet("/usuario")
 public class UsuarioController extends HttpServlet{
+	
+	public UsuarioController (){
+		System.out.println("Contrutor");
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init");
+		super.init();
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		System.out.println("doGet.");
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Usuario usu = new Usuario();
+		usu.setNome(req.getParameter("nome"));
+		usu.setLogin(req.getParameter("login"));
+		usu.setSenha(req.getParameter("senha"));
+		
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.cadastrar(usu);
+		
+		System.out.println("Salvo com sucesso.");
+	}
+	
+	@Override
+	public void destroy() {
 		// TODO Auto-generated method stub
-		//super.doGet(req, resp);
-		System.out.println("chamou");
+		super.destroy();
 	}
 }
--- src/main/java/br/com/fabricadeprogramador/persistencia/jdbc/ConexaoFactory.java
+++ src/main/java/br/com/fabricadeprogramador/persistencia/jdbc/ConexaoFactory.java
@@ -7,8 +7,11 @@ import java.sql.SQLException;
 public class ConexaoFactory {
 	public static Connection getConnection() {
 		try {
+			Class.forName("org.postgresql.Driver");
 			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/fabricaweb", "postgres", "root");
 		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} catch (ClassNotFoundException e){
 			throw new RuntimeException(e);
 		}
 	}
